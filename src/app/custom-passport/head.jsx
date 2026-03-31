43b556cab9d426b9e0bbe156f9c9ac2101a560be
Raphaela Gil <128820385+RaphaGil@users.noreply.github.com>
2026-03-31 12:26:59 +0100

    test

---
+++ src/app/custom-passport/head.jsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="preload"
+        as="image"
+        href="/images/SmartCase/economycaselightpink.webp"
+        fetchPriority="high"
+      />
+    </>
+  );
+}
